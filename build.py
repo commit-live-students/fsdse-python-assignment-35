b3eea9669d697e15ae7dfb1ac568b5125287bed8
ankanroy007 <ankanroy007@users.noreply.github.com>
2017-05-28 23:24:59 +0000

    Done

--- build.py
+++ build.py
@@ -3,4 +3,11 @@ def solution(list1, list2):
     '''
     Enter your code here
     '''
+    for listValue1 in list1:
+        for listValue2 in list2:
+            if listValue1 == listValue2:
+                result = True
+                break
     return result
+
+solution([1,2,3], [2,3])
