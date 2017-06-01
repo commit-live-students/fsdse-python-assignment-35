5ce35f9b112548af72f9ac47bcc026741ec49f92
RupaliHangekar <RupaliHangekar@users.noreply.github.com>
2017-06-01 06:54:20 +0000

    Done

--- build.py
+++ build.py
@@ -1,6 +1,9 @@
 def solution(list1, list2):
     result = False
-    '''
-    Enter your code here
-    '''
+    for x in list1:
+         for y in list2:
+             if x == y:
+                 result = True
     return result
+output = solution ([5,2,3,4],[1,7,8,9,0])
+print output
