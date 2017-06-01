5483a97f14b77d4b3da9e2cddc4291f8e8b1d0dc
Rohan2015 <rohandamodar@outlook.com>
2017-06-01 00:27:44 +0000

    Done

--- build.py
+++ build.py
@@ -1,6 +1,8 @@
 def solution(list1, list2):
     result = False
-    '''
-    Enter your code here
-    '''
+    for i in range(0,len(list1)):
+        for j in range(0,len(list2)):
+            if list1[i] == list2[j]:
+                result = True
+            j = 0
     return result
