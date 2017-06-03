ed6a5da8519161aaa8ea30c2dbd3b8f5c70deda2
coded9 <coded9@users.noreply.github.com>
2017-06-03 09:49:46 +0000

    Done

--- build.py
+++ build.py
@@ -3,4 +3,9 @@ def solution(list1, list2):
     '''
     Enter your code here
     '''
+    for i in list1:
+        if(i in list2):
+            result = True
+            break
     return result
+#print(solution([1,2,3],[5,4,6]))
