85751d4a24f642dd765b7fe214a1794a66b7a6fc
datanomnom <ronzzz92@gmail.com>
2017-06-22 16:24:53 +0000

    Done

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 def solution(list1, list2):
     result = False
-    '''
-    Enter your code here
-    '''
+    for x in list1:
+        if x in list2:
+            result = True
     return result
